c5bddc954a10440f5394a5fc2235b79e512d9532
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-07-21 21:32:54 +0400

    Initial commit

---
+++ src/main/java/DatabaseConnect.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnect {
+
+    StandartServiceRegistry
+
+//    public static Connection getConnection() {
+//        String PASS = "8RCDNjQUwpGg";
+//        String USER = "root";
+//        String URL = "jdbc:mysql://localhost:3306/kata";
+//        Connection connection;
+//        try {
+//            connection = DriverManager.getConnection(URL, USER, PASS);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return connection;
+//    }
+}
