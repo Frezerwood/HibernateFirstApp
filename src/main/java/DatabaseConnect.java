f41653026ef42a9213cafc97e9a20f0213485153
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-07-22 00:08:34 +0400

    SessionFactory

--- src/main/java/DatabaseConnect.java
+++ src/main/java/DatabaseConnect.java
@@ -1,10 +1,24 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.mapping.MetadataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnect {
 
-    StandartServiceRegistry
+    public static SessionFactory getConnect() {
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        return sessionFactory;
+    }
 
 //    public static Connection getConnection() {
 //        String PASS = "8RCDNjQUwpGg";
